38df24a80bcf3249b771708b0bd048f9e311dffc
Diya Khanal <khanal.diya5@gmail.com>
2022-03-03 09:04:37 -0500

    This is the commit that will be marked

---
+++ RazorPagesTarotDecks/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesTarotDecks.Data.RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList< Cards_Detail > Movie { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Themes { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
